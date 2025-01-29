69dfab366a3ce1aad25f0b40e5bebb52456a3592
Ebenezer <ebenezerebi500@gmail.com>
2025-01-30 04:19:29 +0530

    Update Commit

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,6 +4,8 @@ import electronLogo from './assets/electron.svg'
 function App(): JSX.Element {
   return (
     <>
+      <h1>Hello, Electron!</h1>
+      <p>I hope you enjoy using basic-electron-react-boilerplate to build your Electron apps.</p>
       <img alt="logo" className="logo" src={electronLogo} />
       <Versions></Versions>
     </>
