b6987fef05ae4d9bf5afe636574d42624e69fece
Ebenezer <ebenezerebi500@gmail.com>
2025-01-30 03:59:49 +0530

    Updated App

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,15 +4,6 @@ import electronLogo from './assets/electron.svg'
 function App(): JSX.Element {
   return (
     <>
-      <input
-        style={{
-          width: '200px',
-          height: '200px',
-          background: 'red'
-        }}
-      />
-      This is the updated version to show input in the ota
-      <h1>Heyyy This is gonna be the new version</h1>
       <img alt="logo" className="logo" src={electronLogo} />
       <Versions></Versions>
     </>
