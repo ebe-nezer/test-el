42284480151d082d21186f29fbdba89e6f7dbcba
Ebenezer <ebenezerebi500@gmail.com>
2025-01-30 00:33:15 +0530

    New Version

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,6 +4,7 @@ import electronLogo from './assets/electron.svg'
 function App(): JSX.Element {
   return (
     <>
+      <h1>Heyyy This is gonna be the new version</h1>
       <img alt="logo" className="logo" src={electronLogo} />
       <Versions></Versions>
     </>
